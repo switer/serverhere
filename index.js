23c9d3371038fe80b75f999a1debaa8e3ec5e0ca
switer <guankaishe@gmail.com>
2015-01-12 00:10:51 +0800

    update package.json instead of overwrite

--- index.js
+++ index.js
@@ -4,43 +4,57 @@ var Path = require('path')
 var ejs = require('ejs')
 var inquirer = require('inquirer')
 var templates = {
-    "server": fs.readFileSync(Path.join(__dirname, './template/server'), 'utf-8'),
-    "package": fs.readFileSync(Path.join(__dirname, './template/package'), 'utf-8')
+    "server": fs.readFileSync(Path.join(__dirname, './template/server'), 'utf-8')
 }
 
 module.exports = function(path, options) {
     options.port = options.port || 1024
     if (options.engine)!options.views && (options.views = 'views')
 
-    /**
-     *  custom set dependencies
-     **/
-    var deps = '"express": "*"'
-    var spliter = ',\n        '
-
-    // colors
-    deps += spliter + '"colors":"*"'
-    if (options.compress) {
-        deps += spliter + '"compression": "*"'
-    }
-    if (options.engine) {
-        deps += spliter + ejs.render('"<%= engine %>": "*"', options)
-    }
-    if (options.watch) {
-        deps += spliter + '"express-livereload": "*"'
-    }
-    options.deps = deps
 
     var serverPath = 'server.js'
     var packagePath = 'package.json'
     var serverFile = ejs.render(templates.server, options)
-    var packageFile = ejs.render(templates.package, options)
     var existServer = fs.existsSync(serverPath)
     var existPackage = fs.existsSync(packagePath)
     var questions = []
     var writeServer = true
-    var writePackage = true
+    
+    /**
+     *  set package.json
+     */
+    var packageJSON
+    try {
+        if (existPackage) {
+            packageJSON = (packageJSON = fs.readFileSync('package.json', 'utf-8')) ? JSON.parse(packageJSON):{}
+        } else {
+            packageJSON = {}
+        }
+    } catch (e) {
+        console.log('Your "package.json" file has syntax error : ' + e);
+        return process.exit(1)
+    }
+    if (!packageJSON.name) packageJSON.name = Path.basename(process.cwd()) || ''
+    !packageJSON.scripts && (packageJSON.scripts = {})
+    packageJSON.scripts.start = 'node server'
 
+    !packageJSON.dependencies && (packageJSON.dependencies = {})
+
+    // custom dependencies
+    var deps = ['express', 'colors']
+    switch (true) {
+        case !!options.compress: deps.push('compression');;
+        case !!options.engine: deps.push(options.engine);;
+        case !!options.watch: deps.push('express-livereload');;
+    }
+    deps.forEach(function (dep) {
+        packageJSON.dependencies[dep] = '*'
+    })
+    fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, '\t'))
+
+    /**
+     *  write server.js file
+     */
     if (existServer) {
         questions.push({
             type: "confirm",
@@ -48,34 +62,19 @@ module.exports = function(path, options) {
             message: "server.js is already exist, still rewrite?",
             default: true
         })
-    }
-    if (existPackage) {
-        questions.push({
-            type: "confirm",
-            name: "writePackage",
-            message: "package.json is already exist, still rewrite?",
-            default: true
-        })
-    }
-    if (existServer || existPackage) {
         inquirer.prompt(questions, function (answers) {
             existServer && (writeServer = answers.writeServer)
-            existPackage && (writePackage = answers.writePackage)
-            writeFiles()
+            writeServerFile()
         })
     } else {
-        writeFiles()
+        writeServerFile()
     }
-    function writeFiles () {
+    function writeServerFile () {
         if (writeServer) {
             console.log('\nDone, server.js is created!')
             fs.writeFileSync('server.js', serverFile, 'utf-8')
         }
-        if (writePackage) {
-            console.log('\nDone, package.json is created!')
-            fs.writeFileSync('package.json', packageFile, 'utf-8')
-        }
-
+        console.log('Done, ' + (existPackage ? 'package.json is updated!':'package.json is created!'))
         console.log('\nPlease use "npm install && npm start" to run server.')
     }
 }
