45f7313e72a7eb2da42d0a74ce6c4e443218261a
switer <guankaishe@gmail.com>
2015-02-06 11:10:56 +0800

    set index.html template title to cwd.

--- index.js
+++ index.js
@@ -16,60 +16,59 @@ module.exports = function(path, options) {
 
     var serverPath = 'server.js'
     var packagePath = 'package.json'
-    var IndexPath = Path.join(options.public, 'index.html')
-    var serverFile = ejs.render(templates.server, options)
-    var existServer = fs.existsSync(serverPath)
+    var indexPath = Path.join(options.public, 'index.html')
     var existPackage = fs.existsSync(packagePath)
-    var existIndex = fs.existsSync(IndexPath)
-    var questions = []
-    var writeServer = true
+    var existIndex = fs.existsSync(indexPath)
 
     /**
      * write static folder and default index.html
      */
-    if(options.public !== '.') mkdirp.sync(options.public);
+    if (options.public !== '.') mkdirp.sync(options.public);
+    options.cwd = Path.basename(process.cwd()) || ''
 
-    if(!existIndex){
-        fs.writeFileSync(IndexPath, templates.index, 'utf-8')
-        console.log('Done, index.html is created!')   
+    if (!existIndex) {
+        fs.writeFileSync(indexPath, ejs.render(templates.index, options), 'utf-8')
+        console.log('Done, index.html is created!')
     }
-    
+
     /**
      *  set package.json
      */
-    var packageJSON
+    var pjson
     if (existPackage) {
         try {
-            packageJSON = (packageJSON = fs.readFileSync('package.json', 'utf-8')) ? JSON.parse(packageJSON):{}
+            pjson = (pjson = fs.readFileSync('package.json', 'utf-8')) ? JSON.parse(pjson) : {}
         } catch (e) {
             console.log('Your "package.json" file has syntax error : ' + e)
             return process.exit(1)
         }
     } else {
-        packageJSON = {}
+        pjson = {}
     }
 
-    packageJSON.name === undefined && (packageJSON.name = Path.basename(process.cwd()) || '')
-    packageJSON.scripts = packageJSON.scripts || {}
-    packageJSON.scripts.start = 'node server'
-    packageJSON.dependencies = packageJSON.dependencies || {}
+    pjson.name === undefined && (pjson.name = options.cwd)
+    pjson.scripts = pjson.scripts || {}
+    pjson.scripts.start = 'node server'
+    pjson.dependencies = pjson.dependencies || {}
 
     // custom dependencies
     var deps = ['express', 'colors']
-    switch (true) {
-        case !!options.compress: deps.push('compression')
-        case !!options.engine: deps.push(options.engine)
-        case !!options.watch: deps.push('express-livereload')
-    }
-    deps.forEach(function (dep) {
-        packageJSON.dependencies[dep] = '*'
+    if (options.compress) deps.push('compression')
+    if (options.engine) deps.push(options.engine)
+    if (options.watch) deps.push('express-livereload')
+
+    deps.forEach(function(dep) {
+        pjson.dependencies[dep] = '*'
     })
-    
-    fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, '\t'))
+
+    fs.writeFileSync('package.json', JSON.stringify(pjson, null, '\t'))
 
     /**
      *  write server.js file
      */
+    var questions = []
+    var writeServer = true
+    var existServer = fs.existsSync(serverPath)
     if (existServer) {
         questions.push({
             type: "confirm",
@@ -77,19 +76,22 @@ module.exports = function(path, options) {
             message: "server.js is already exist, still rewrite?",
             default: true
         })
-        inquirer.prompt(questions, function (answers) {
+        inquirer.prompt(questions, function(answers) {
             existServer && (writeServer = answers.writeServer)
             writeServerFile()
         })
     } else {
         writeServerFile()
     }
-    function writeServerFile () {
+
+    var serverFile = ejs.render(templates.server, options)
+
+    function writeServerFile() {
         if (writeServer) {
             console.log('\nDone, server.js is created!')
             fs.writeFileSync('server.js', serverFile, 'utf-8')
         }
-        console.log('Done, ' + (existPackage ? 'package.json is updated!':'package.json is created!'))
+        console.log('Done, ' + (existPackage ? 'package.json is updated!' : 'package.json is created!'))
         console.log('\nPlease use "npm install && npm start" to run server.')
     }
 }
